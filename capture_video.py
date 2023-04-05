954dd91a975e3a64e6e2208b460a75a04f6d0eb3
Zort <joseperdomo2001@outlook.com>
2023-04-04 23:05:19 -0500

    codigo video

---
+++ capture_video.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+while True:
+
+    ret,frame = cap.read()
+    
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == ord('a'):
+        break
+cap.release()
+cv2.destroyAllWindows
